54c447328e6ca3a6405d765fe92f4bcb3b7d03a0
Piotr Grzesik <pj.grzesik@gmail.com>
2018-05-24 22:16:20 +0200

    Refactor out Option struct

--- story.go
+++ story.go
@@ -3,8 +3,10 @@ package cyoa
 type Chapter struct {
 	Title      string   `json:"title"`
 	Paragraphs []string `json:"story"`
-	Options    []struct {
-		Text string `json:"text"`
-		Arc  string `json:"arc"`
-	} `json:"options"`
+	Options    []Option `json:"options"`
+}
+
+type Option struct {
+	Text    string `json:"text"`
+	Chapter string `json:"arc"`
 }
