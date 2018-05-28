8d2d240e8564f556033111717eaa98d99ac3f54f
Piotr Grzesik <pj.grzesik@gmail.com>
2018-05-29 00:10:53 +0200

    Fix template

--- story.go
+++ story.go
@@ -12,17 +12,18 @@ var defaultHandlerTmpl = `
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Choose Your Own Adventure</html>
+    <title>Choose Your Own Adventure</title>
   </head>
   <body>
-    <h1>{{.Title}}</h1>
-    {{range .Paragraphs}}
-      <p>{{.}}</p>
-    {{end}}
-    <ul>
-    {{range .Options}}
-      <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
-    </ul>
+      <h1>{{.Title}}</h1>
+      {{range .Paragraphs}}
+        <p>{{.}}</p>
+      {{end}}
+      <ul>
+      {{range .Options}}
+        <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+      {{end}}
+      </ul>
   </body>
 </html>`
 
